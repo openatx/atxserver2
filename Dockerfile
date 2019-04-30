83f07a6aea4f71b91b72c894d13c5e0619e35228
codeskyblue <codeskyblue@gmail.com>
2019-04-30 11:03:36 +0800

    use rethinkdb lib from pypi, fix docker-compose up failed bug

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ ADD . /app
 WORKDIR /app
 
 RUN pip install -r requirements.txt
-RUN pip install -r requirements-dev.txt
 
 ENTRYPOINT [ "scripts/wait-for-db.sh" ]
 CMD ["python", "main.py"]
--- scripts/wait_for_rethinkdb.py
+++ scripts/wait_for_rethinkdb.py
@@ -12,8 +12,10 @@ from rethinkdb import r
 deadline = time.time() + 10
 while time.time() < deadline:
     try:
-        r.connect(os.environ.get("RDB_HOST", "localhost"))
+        conn = r.connect(os.environ.get("RDB_HOST", "localhost"))
         print("RethinkDB is running")
+        res = r.db_list().run(conn)
+        assert isinstance(res, list)
         sys.exit(0)
     except Exception as e:
         time.sleep(.2)
